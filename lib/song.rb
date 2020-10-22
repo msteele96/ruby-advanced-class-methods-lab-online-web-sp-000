cc0d378d6a344add932863783eafc1c4c7c66248
Marc Steele <marcsteele2014@gmail.com>
2020-10-22 23:01:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -24,22 +24,25 @@ class Song
   end
 
   def self.create_by_name(name)
-    song = self.new
+    song = self.create
     song.name = name
-    self.all << song
+    # self.all << song
     return song
   end
 
   def self.find_by_name(name)
+    binding.pry
     self.all.find {|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
+    # binding.pry
     if self.find_by_name(name) == nil
       self.create_by_name(name)
     else
       self.find_by_name(name)
     end
+    # self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
